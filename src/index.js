bfa653510aa889446564147222173f5ca857c104
Kaushal Banthia <62059447+kaushal-banthia@users.noreply.github.com>
2021-06-18 23:13:48 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react"
+import reactDom from "react-dom"
+
+const root = document.getElementById("root");
+const heading = <h1>Hello from create react app</h1>
+ReactDOM.render(heading, root);
